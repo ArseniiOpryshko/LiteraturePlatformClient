cd55e59efab5d2aeafa19efe525bd88ae3136dca
Phanttt <117937309+Phanttt@users.noreply.github.com>
2023-01-25 16:23:15 +0100

    Добавьте файлы проекта.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace LiteraturePlatformClient.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
